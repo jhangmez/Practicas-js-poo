04142df89e8b1bb6a15a1ff154323483fa6fd0da
jhangmez <jhangomez25@gmail.com>
2022-12-26 19:05:19 -0500

    Agregue varias cositas 🥰v2

---
+++ main.js
@@ -0,0 +1,39 @@
+// Objetos literales y prototipos en JavaScript
+
+/*
+https://platzi.com/comentario/2667411/
+*/
+
+// const natalia = {
+//     name: "Natalia",
+//     age: 20,
+//     cursosAprobados: [
+//         "Curso definitivo de HTML y CSS",
+//         "Curso Practico de HTML y CSS",
+//         "Curso de Analisis de amenazas",
+//     ],
+// };
+
+// // Natalia quiere aprobar otro curso
+// natalia.cursoAprobados.push("Curso de Responsive Designe");
+// console.log(natalia.cursoAprobados);
+
+function Student (name, age, cursosAprobados){
+    this.name=name;
+    this.age=age;
+    this.cursosAprobados = cursosAprobados;
+}
+
+Student.prototype.aprobarCurso = function(nuevoCurso){
+    this.cursosAprobados.push(nuevoCurso);
+}
+
+const juanillo = new Student(
+    "Juanillo Perez",
+    '22',
+    [
+        "Introduccion a la Ingenieria de Sistemas",
+        "Introduccion a Base de datos",
+        "Fundamentos del Pentesting"
+    ],
+);
